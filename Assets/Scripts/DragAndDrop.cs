00365df4b721ede735571967daf8372372ff3e98
santanist97@gmail.com <santanist97@gmail.com>
2020-03-17 20:00:15 +0500

    first commit

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private GameObject _drag;
+    private Vector3 screenPosition;
+    private Vector3 offset;
+
+    private void Update()
+    {
+        if(_drag == null && Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if(gameObject == hit.transform.gameObject)
+                {
+                    _drag = hit.transform.gameObject;
+                    screenPosition = Camera.main.WorldToScreenPoint(_drag.transform.position);
+                    Vector3 point = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPosition.z);
+                    offset = _drag.transform.position - Camera.main.ScreenToWorldPoint(point);
+                }
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            _drag = null;
+        }
+
+        if (_drag != null)
+        {
+            Vector3 currentScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPosition.z);
+            Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenSpace) + offset;
+            _drag.transform.position = new Vector3(currentPosition.x, _drag.transform.position.y, currentPosition.z);
+        }
+    }
+}
---
+++ Assets/Scripts/Magnet.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Magnet : MonoBehaviour
+{
+    public float magnetForce = 100;
+
+    List<Rigidbody> caughtRigidbodies = new List<Rigidbody>();
+
+    void FixedUpdate()
+    {
+        for (int i = 0; i < caughtRigidbodies.Count; i++)
+        {
+            caughtRigidbodies[i].velocity = (transform.position - (caughtRigidbodies[i].transform.position + caughtRigidbodies[i].centerOfMass)) * magnetForce * Time.deltaTime;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Rigidbody>())
+        {
+            Rigidbody r = other.GetComponent<Rigidbody>();
+
+            if (!caughtRigidbodies.Contains(r))
+            {
+                caughtRigidbodies.Add(r);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Rigidbody>())
+        {
+            Rigidbody r = other.GetComponent<Rigidbody>();
+
+            if (caughtRigidbodies.Contains(r))
+            {
+                caughtRigidbodies.Remove(r);
+            }
+        }
+    }
+}
+
